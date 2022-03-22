2dbdf88bac0b0a115a2d2e2fd3a9c3a6a93e4a0a
Sandeep03edu <sandeep03edu@gmail.com>
2022-03-22 15:21:08 +0530

    Added Hospital, Doctor Modal class and AppointmentSchedulerActivity.java

---
+++ app/src/main/java/com/techmeet/mercari/Medical/AppointmentSchedulerActivity.java
@@ -0,0 +1,54 @@
+package com.techmeet.mercari.Medical;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import com.google.android.material.tabs.TabLayout;
+import com.techmeet.mercari.R;
+import com.techmeet.mercari.Utils.ViewPagerAdapter;
+
+public class AppointmentSchedulerActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_scheduler);
+
+        _init();
+
+        // Setting ViewPager
+        SetViewPager();
+
+    }
+
+    public String getEdittextText(){
+        if(editText==null){
+            return "";
+        }
+        return editText.getText().toString().trim();
+    }
+
+    private void SetViewPager() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HospitalFragment(), "Hospital");
+        adapter.addFragment(new DoctorFragment(), "Doctor");
+        viewPager.setAdapter(adapter);
+        viewPager.setOffscreenPageLimit(2);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void _init() {
+        tabLayout = findViewById(R.id.appointment_scheduler_tab_layout);
+        viewPager = findViewById(R.id.appointment_scheduler_view_pager);
+        editText =findViewById(R.id.appointment_scheduler_edittext);
+    }
+}
---
+++ app/src/main/java/com/techmeet/mercari/Medical/HospitalFragment.java
@@ -0,0 +1,80 @@
+package com.techmeet.mercari.Medical;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.techmeet.mercari.R;
+
+
+public class HospitalFragment extends Fragment {
+    public HospitalFragment() {
+        // Required empty public constructor
+    }
+
+    boolean isVisible = false;
+    EditText searchText;
+    RecyclerView recyclerView;
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        isVisible = isVisibleToUser;
+        if(isVisible){
+            GetData();
+        }
+    }
+
+    private void GetData() {
+        // TODO: Implement API and adapter
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_hospital, container, false);
+
+        _init(view);
+
+        AppointmentSchedulerActivity activity = (AppointmentSchedulerActivity) getActivity();
+        if(activity!=null) {
+            searchText = activity.editText;
+            searchText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    if(isVisible){
+                        // Refresh Data if visible to user
+                        GetData();
+                    }
+                }
+            });
+        }
+
+        return view;
+    }
+
+    private void _init(View v) {
+        recyclerView = v.findViewById(R.id.fragment_hospital_rcv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+}
